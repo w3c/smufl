b7a48e728ebaadecc2f195a0f5c01ff8383af3b4
Jeff Kellem <github-dist@beyond.dreams.org>
2019-02-13 11:06:22 -0800

    cutOut anchors defined from origin in SMuFL v1.3, so no special case needed anymore

--- scripts/fontlab/generate_font_metadata.py
+++ scripts/fontlab/generate_font_metadata.py
@@ -97,14 +97,8 @@ for g in fl.font.glyphs:
     if len(g.anchors) > 0:
         font_metadata['glyphsWithAnchors'][g.note] = {}
         for anchor in g.anchors:
-
-            if anchor.name.startswith('cutOut'):
-                fontlab_metadata['glyphsWithAnchors'][g.note][anchor.name] = \
-                    [to_cartesian(anchor.x - bounding_box.ll.x),
-                     to_cartesian(anchor.y - bounding_box.ll.y)]
-            else:
-                fontlab_metadata['glyphsWithAnchors'][g.note][anchor.name] = \
-                    [to_cartesian(anchor.x), to_cartesian(anchor.y)]
+            fontlab_metadata['glyphsWithAnchors'][g.note][anchor.name] = \
+                [to_cartesian(anchor.x), to_cartesian(anchor.y)]
 
     if int(g.unicode) >= 0xF400:
         font_metadata["optionalGlyphs"][g.note] = {'classes': []}
